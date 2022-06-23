add114663cb536c2096e1fbda2cc6ced39f2d6a0
ReneSena <rene.sena3005@gmail.com>
2022-06-23 11:25:16 -0300

    Organizing tests

--- src/components/TextField/TextField.test.tsx
+++ src/components/TextField/TextField.test.tsx
@@ -16,46 +16,50 @@ describe('<TextField />', () => {
         jest.clearAllMocks();
     });
 
-    it('should render TextField Component', () => {
-        renderComponent();
+    describe('Input to write new tasks', () => {
+        it('should render TextField Component', () => {
+            renderComponent();
 
-        const form = screen.getByRole('form');
-        const inputField = screen.getByPlaceholderText('Add a new task');
-        const addButton = screen.getByRole('button', { name: /add/i });
+            const form = screen.getByRole('form');
+            const inputField = screen.getByPlaceholderText('Add a new task');
+            const addButton = screen.getByRole('button', { name: /add/i });
 
-        expect(form).toBeInTheDocument();
-        expect(inputField).toBeInTheDocument();
-        expect(addButton).toBeInTheDocument();
+            expect(form).toBeInTheDocument();
+            expect(inputField).toBeInTheDocument();
+            expect(addButton).toBeInTheDocument();
+        });
+
+        it('should render TextField Component with type text', () => {
+            renderComponent();
+
+            const inputField = screen.getByPlaceholderText('Add a new task');
+
+            expect(inputField).toHaveProperty('type', 'text');
+        });
+
+        it('should render a value in TextField Component and execute The onChange function', async () => {
+            renderComponent('To do coffee');
+
+            const inputField = screen.getByPlaceholderText('Add a new task');
+            await userEvent.type(inputField, 'To do coffee');
+
+            expect(inputField).toHaveValue('To do coffee');
+            expect(mockOnChange).toBeCalledTimes(12);
+        });
     });
 
-    it('should render TextField Component with type text', () => {
-        renderComponent();
+    describe('Button to add new tasks', () => {
+        it('should call onSubmit clicking in [Add Button], after the user fills input', async () => {
+            renderComponent('To do coffee');
 
-        const inputField = screen.getByPlaceholderText('Add a new task');
+            const inputField = screen.getByPlaceholderText('Add a new task');
+            await userEvent.type(inputField, 'To do coffee');
 
-        expect(inputField).toHaveProperty('type', 'text');
-    });
+            const addButton = screen.getByRole('button', { name: /add/i });
+            await userEvent.click(addButton);
 
-    it('should render a value in TextField Component and execute The onChange function', async () => {
-        renderComponent('To do coffee');
-
-        const inputField = screen.getByPlaceholderText('Add a new task');
-        await userEvent.type(inputField, 'To do coffee');
-
-        expect(inputField).toHaveValue('To do coffee');
-        expect(mockOnChange).toBeCalledTimes(12);
-    });
-
-    it('should call onSubmit clicking in [Add Button], after the user fills input', async () => {
-        renderComponent('To do coffee');
-
-        const inputField = screen.getByPlaceholderText('Add a new task');
-        await userEvent.type(inputField, 'To do coffee');
-
-        const addButton = screen.getByRole('button', { name: /add/i });
-        await userEvent.click(addButton);
-
-        expect(mockOnChange).toBeCalledTimes(12);
-        expect(mockOnSubmit).toBeCalledTimes(1);
+            expect(mockOnChange).toBeCalledTimes(12);
+            expect(mockOnSubmit).toBeCalledTimes(1);
+        });
     });
 });
