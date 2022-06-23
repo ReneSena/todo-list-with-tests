f540fe4cc2b9ab453e2d2f3729058d75b9c2d236
ReneSena <rene.sena3005@gmail.com>
2022-06-23 00:28:39 -0300

    Adding config coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,20 @@
-import type { Config } from "jest";
+import type { Config } from 'jest';
 
 export default async (): Promise<Config> => {
-  return {
-    verbose: true,
-    roots: ["<rootDir>/src"],
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    testEnvironment: "jsdom",
-  };
+    return {
+        verbose: true,
+        roots: ['<rootDir>/src'],
+        testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+        testEnvironment: 'jsdom',
+        collectCoverageFrom: ['**/*.{ts,tsx}', '!**/node_modules/**'],
+        coverageThreshold: {
+            global: {
+                branches: 90,
+                functions: 90,
+                lines: 90,
+                statements: 90,
+            },
+        },
+    };
 };
+
