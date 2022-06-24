693adbf2016f4309f7f730177f42f57d67d3c6be
ReneSena <rene.sena3005@gmail.com>
2022-06-23 22:56:17 -0300

    Logic to add new task's

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { ITodoListItem, TodoList } from './components/TodoList/TodoList';
 import React from 'react';
 import { TextField } from './components/TextField/TextField';
 
 function App() {
     const [inputFieldValue, setInputFieldValue] = React.useState<string>('');
+    const [tasks, setTasks] = React.useState<ITodoListItem[]>([]);
 
     const handleOnChange = (event: React.BaseSyntheticEvent) =>
         setInputFieldValue(event.target.value);
@@ -10,12 +12,26 @@ function App() {
     const handleAddNewTask = (event: React.BaseSyntheticEvent) => {
         event.preventDefault();
 
+        const listItems = {
+            id: Math.random().toString(),
+            value: inputFieldValue,
+        };
+
+        setTasks([...tasks, listItems]);
         setInputFieldValue('');
     };
 
     return (
-        <TextField value={inputFieldValue} onChange={handleOnChange} onSubmit={handleAddNewTask} />
+        <>
+            <TextField
+                value={inputFieldValue}
+                onChange={handleOnChange}
+                onSubmit={handleAddNewTask}
+            />
+            <TodoList listItems={tasks} />
+        </>
     );
 }
 
 export default App;
+
